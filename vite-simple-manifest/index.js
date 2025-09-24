b2997b5eabdb84fa9376fd5d31fa985c2ad3910a
NiclasNorin <niclasn@outlook.com>
2025-09-24 16:33:03 +0200

    refactor: warning when faulty js entry

--- vite-simple-manifest/index.js
+++ vite-simple-manifest/index.js
@@ -8,11 +8,13 @@ export function manifestPlugin(outputFile = 'manifest.json') {
           let baseName = fileName.replace('.css', '')
           if (baseName.includes('.')) baseName = baseName.replace(/\.[a-zA-Z0-9_-]+$/, '')
           manifest[baseName + '.css'] = fileName
-        } else if (chunk.type === 'chunk' && fileName.endsWith('.js')) {
+        } else if (chunk.type === 'chunk' && fileName.endsWith('.js') && chunk.facadeModuleId && !/\.(css|scss|sass)$/.test(chunk.facadeModuleId)) {
           let baseName = fileName.replace('.js', '')
           if (baseName.includes('.')) baseName = baseName.replace(/\.[a-zA-Z0-9_-]+$/, '')
           const entryKey = baseName + '.js'
-          if (entryKey.startsWith('js/')) manifest[entryKey] = fileName
+          if (entryKey.startsWith('js/')) { manifest[entryKey] = fileName } else {
+            this.warn(`Skipping JS entry that does not start with "js/": ${chunk.fileName}`)
+          }
         }
       }
       this.emitFile({
